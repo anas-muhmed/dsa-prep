6a62909512b463d5e87aa8448bed5c1e5b25baf2
0x-Anas <muhammedanasoff@gmail.com>
2026-05-02 19:53:17 +0530

    readable clean pyrmid code solution

--- patterns/)4_pyramid_traingle.py
+++ patterns/)4_pyramid_traingle.py
@@ -1,4 +1,9 @@
-row=int(input("enter the rows: "))
+row = int(input("Enter number of rows: "))
 
-for i in range(1,row+1):
-    print(" " * (row-i)+ "*" * (2*i-1))
+if row <= 0:
+    print("Invalid input")
+else:
+    for i in range(1, row + 1):
+        spaces = row - i
+        stars = 2 * i - 1
+        print(" " * spaces + "*" * stars)
