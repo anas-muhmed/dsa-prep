194fbf3cb254d40f76399103dab7beecef7847fa
0x-Anas <muhammedanasoff@gmail.com>
2026-05-03 19:57:45 +0530

    corrected version

--- strings/)9_remove_duplicate.py
+++ strings/)9_remove_duplicate.py
@@ -3,6 +3,6 @@ seen=set()
 result=[]
 for char in s:
     if char not in seen:
-        seen.append(char)
+        seen.add(char)
         result.append(char)
-print("".join(seen))
+print("".join(result))
